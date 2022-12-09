e8b64252ca4548e5c154d8006e5422cd3de35ca7
Kevin Janisch <kevin91@gmx.net>
2022-12-09 23:06:53 +0100

    pisolarcontroller

--- SolarControllerMqttClient/config.py
+++ SolarControllerMqttClient/config.py
@@ -3,7 +3,6 @@ import os
 mountpoint=os.getenv('mountpoint', '/mnt/usb')
 basepath=f'{mountpoint}/LOGFILES/'
 
-sshcommand=os.getenv('sshcommand', 'ssh -o "StrictHostKeyChecking=no" root@openwrt.fritz.box')
 csvheader = os.getenv('csvheader', 'Time	TS1 [°C]	TS2 [°C]	TS3 [°C]	TS4 [°C]	TS5 [°C]	TS6 [°C]	UI1 [°C]	RO1 [%]	RO2 [%]	REL [%]	RO1 Safety	RO2 Safety	REL Safety	Vol. flow	Vol. flow 3	Vol. flow 4	Heat quantity	Heat quantity 1	Heat quantity 2	Heat quantity 3	Heat quantity 4	CO2 Savings	Errors')
 
 
--- SolarControllerMqttClient/sshclient.py
+++ SolarControllerMqttClient/sshclient.py
@@ -4,19 +4,19 @@ import sys
 import config
 import log
 
-logger = log.get_module_logger("sshclient")
+logger = log.get_module_logger("client")
 latestTime = ''
 
 # Creating mountpoint
 def init():
     try:
         # Checking if folder mountpoint (0) if mountpoint exists
-        mountpointExists = subprocess.check_output(f'{config.sshcommand} "test -d {config.mountpoint} ; echo $?"', shell=True).decode().strip() == '0'
+        mountpointExists = subprocess.check_output(f'test -d {config.mountpoint} ; echo $?', shell=True).decode().strip() == '0'
         if mountpointExists:
             logger.info("sshclient initialized and ready...")
             return
 
-        subprocess.check_output(f'{config.sshcommand} "mkdir /mnt/usb"', shell=True)
+        subprocess.check_output(f'mkdir {config.mountpoint}', shell=True)
         logger.info("sshclient initialized and ready...")
     except Exception as e:
         logger.critical(f"Failed to initialize. No data can be retrieved : {e}")
@@ -27,14 +27,14 @@ def getData():
     try:
         # Mounting the sdcard
         logger.debug("Mounting /dev/sda1...")
-        subprocess.check_output(f'{config.sshcommand} "mount /dev/sda1 {config.mountpoint}"', shell=True)
+        subprocess.check_output(f'mount /dev/sda1 {config.mountpoint}', shell=True)
 
         # Getting last filename in the folder (latest)
-        latestFilename=subprocess.check_output(f'{config.sshcommand} "ls {config.basepath} | grep DAT | tail -n 1"', shell=True).decode().strip()
+        latestFilename=subprocess.check_output(f'ls {config.basepath} | grep DAT | tail -n 1', shell=True).decode().strip()
         logger.debug(f"Latest filename: {latestFilename}")
 
         # Getting LAST line in the file
-        content=subprocess.check_output(f'{config.sshcommand} "cat {config.basepath + latestFilename} | tail -n 1"', shell=True).decode().strip()
+        content=subprocess.check_output(f'cat {config.basepath + latestFilename} | tail -n 1', shell=True).decode().strip()
         logger.debug(f"Retrieved content: {content}")
 
 
@@ -46,7 +46,7 @@ def getData():
         try:
             # Unmounting the sdcard
             logger.debug("Unmounting /dev/sda1...")
-            subprocess.check_output(f'{config.sshcommand} "umount /mnt/usb"', shell=True)
+            subprocess.check_output(f'umount /mnt/usb', shell=True)
         except Exception as e:
             logger.error(f"Unable to unmount {e}")
 
